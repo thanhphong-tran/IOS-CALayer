168ff62033b11f29414513220a770f1ff59f8771
Thanh-Phong Tran <ttphong2608@gmail.com>
2017-04-13 09:19:05 +0700

    finish

--- layers/ViewController.swift
+++ layers/ViewController.swift
@@ -9,26 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet private var box: UIView!
+    @IBOutlet fileprivate var box: UIView!
     
     override func viewDidLoad() {
-        // Creating rounder corners
-        box.layer.cornerRadius = 10
+        box.layer.cornerRadius = 5;
         
-        // Adding shadow effects
-        box.layer.shadowOffset = CGSizeMake(5, 5)
+        box.layer.shadowOffset = CGSize(width: 5, height: 5)
         box.layer.shadowOpacity = 0.7
         box.layer.shadowRadius = 5
-        box.layer.shadowColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0).CGColor
+        box.layer.shadowColor = UIColor(red: 44.0/255, green: 62.0/255, blue: 80.0/255, alpha: 1.0).cgColor
         
-        // Applying borders
-        box.layer.borderColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0).CGColor
+        box.layer.borderColor = UIColor.blue.cgColor
         box.layer.borderWidth = 3
         
-        // Display images
-        box.layer.contents = UIImage(named: "tree.jpg")?.CGImage
+        box.layer.contents = UIImage(named: "tree.jpg")?.cgImage
         box.layer.contentsGravity = kCAGravityResize
-        box.layer.masksToBounds = true
-
+        box.layer.masksToBounds = false
+        
+        box.layer.backgroundColor = UIColor.blue.cgColor
+        box.layer.opacity = 0.5
+        
+        // True: draw layer on background thread
+        // Use: View needs to redraw frequently
+        box.layer.drawAsynchronously = true
+        
+        // True: the layer is draw once, store bitmap for redrawing.
+        // Use: View doesn't need to redraw frequently
+        box.layer.shouldRasterize = false
     }
 }
